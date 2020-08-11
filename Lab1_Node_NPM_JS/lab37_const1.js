186fc7ef3871013621014f500fa5ea38cc9c5c63
kc <kc@chtti302>
2020-08-11 14:46:46 +0800

    commit 1446

---
+++ Lab1_Node_NPM_JS/lab37_const1.js
@@ -0,0 +1,25 @@
+const PROGRAM_TITLE = "hello"
+//PROGRAM_TITLE += " world"
+let MY_TITLE = "hello"
+MY_TITLE += " world"
+console.log(PROGRAM_TITLE, MY_TITLE)
+const MAX1 = 1
+let i = 100
+const MAX2 = i + 50
+i += 200
+console.log(MAX1, MAX2, i)
+const array1 = [1, 2, 3]
+console.log(array1)
+array1.push(4)
+array1.push(5)
+array1.push(6)
+console.log(array1)
+let array2 = [1,2,3]
+console.log(array2)
+array2.push(4)
+array2.push(5)
+array2.push(6)
+console.log(array2)
+array2 = ['p','q','r']
+console.log(array2)
+//array1 = ['p','q','r']
