3527da3e23b464611c60a8045c35489656fc51fd
kc <kc@chtti302>
2020-08-17 15:23:17 +0800

    class with class property and babel plugins

---
+++ Lab2_babel_modern_js_syntax/src/lab4_class2.js
@@ -0,0 +1,14 @@
+class Course {
+    duration = 49
+    printDuration = () => {
+        return `duration = ${this.duration}`
+    }
+}
+class ReactCourse extends Course {
+    name = 'React'
+    printCourse = () => {
+        return `course name = ${this.name}`
+    }
+}
+const c1 = new ReactCourse()
+console.log(`c1 course ${c1.printDuration()}, ${c1.printCourse()}`)
