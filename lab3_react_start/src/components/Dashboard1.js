a8f75502758fd68e594996e178f92151e57903a4
kc <kc@chtti302>
2020-08-18 10:27:27 +0800

    class based v.s. functional  based component

---
+++ lab3_react_start/src/components/Dashboard1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Dashboard1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+            </div>
+        )
+    }
+}
---
+++ lab3_react_start/src/components/Person.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Person = () => <p>I am groot</p>
+
+
+export default Person
