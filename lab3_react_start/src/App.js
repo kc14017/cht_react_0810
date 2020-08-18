0ec73216df1acba84001cfa046218c1a30630d09
kc <kc@chtti302>
2020-08-18 16:56:05 +0800

    apply style with condition

--- lab3_react_start/src/App.js
+++ lab3_react_start/src/App.js
@@ -38,11 +38,20 @@ class App extends Component {
     })
   }
   toggleDisplayHandler = () => {
-    this.setState({ showPersons: !this.state.showPersons })
+    this.setState({ showPersons: !this.state.showPersons }, () => {
+      console.log(`current status = ${this.state.showPersons}`)
+    })
+  }
+  deletePersonHandler = personIndex => {
+    //const persons = this.state.persons
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
   }
   render() {
     const style = {
-      background: "yellow",
+      backgroundColor: "green",
+      color:'white',
       font: "inherit",
       border: "2px solid red",
       padding: "4px",
@@ -51,10 +60,15 @@ class App extends Component {
     let persons = null;
     if (this.state.showPersons === true) {
       persons = (<div>{
-        this.state.persons.map(person => {
-          return <Person name={person.name} age={person.age} />
+        this.state.persons.map((person, index) => {
+          return <Person
+            key={index}
+            clickCallback={() => this.deletePersonHandler(index)}
+            name={person.name} age={person.age} />
         })
       }</div>)
+      style.backgroundColor = 'red'
+      style.color='black'
     }
     return (
       <div className="App">
