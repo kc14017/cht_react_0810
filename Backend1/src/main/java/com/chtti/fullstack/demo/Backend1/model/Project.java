f6031fff0a4a33e17da2d98d3902cffc114764d6
kc <kc@chtti302>
2020-08-24 17:00:27 +0800

    raise exception when  invalid input

--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/model/Project.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/model/Project.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -19,23 +21,30 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "project name is required")
     private String projectName;
+    @NotBlank(message = "project identifier is required")
+    @Size(min = 6, max = 16, message = "project identifier should within 6 to 16 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "description is required")
     private String description;
-    @JsonFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date startDate;
-    @JsonFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date endDate;
-    @JsonFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date createdAt;
-    @JsonFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date updatedAt;
+
     @PrePersist
     protected void onCreate() {
         this.createdAt = new Date();
     }
+
     @PreUpdate
-    protected void onUpdate(){
+    protected void onUpdate() {
         this.updatedAt = new Date();
     }
 
