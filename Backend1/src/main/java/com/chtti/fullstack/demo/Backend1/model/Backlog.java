ab13e99c4e6f63f23294ac3bac374fee545636a3
kc <kc@chtti302>
2020-08-25 17:01:40 +0800

    commit 1701

--- Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/model/Backlog.java
+++ Backend1/src/main/java/com/chtti/fullstack/demo/Backend1/model/Backlog.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -19,4 +21,7 @@ public class Backlog {
     @JoinColumn(name = "project_id", nullable = false)
     @JsonIgnore
     private Project project;
+    @OneToMany(cascade = CascadeType.REMOVE, orphanRemoval = true,
+            fetch = FetchType.EAGER, mappedBy = "backlog")
+    private List<ProjectTask> projectTask = new ArrayList<>();
 }
